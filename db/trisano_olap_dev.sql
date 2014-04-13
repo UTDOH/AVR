4a8aa0b906ee405a3eb93bea2919294864759c64
developer <developer@developer.(none)>
2014-04-12 21:02:58 -0600

    added Lab Test cube

--- db/trisano_olap_dev.sql
+++ db/trisano_olap_dev.sql
@@ -63,15 +63,45 @@ CREATE TABLE warehouse.dim_person
 alter table warehouse.dim_person add primary key (dw_person_id);
 
 
+
 create sequence warehouse.dim_junk_lab_test_seq;
 CREATE TABLE warehouse.dim_junk_lab_test
 (
   dw_junk_lab_test_id int not null default nextval('warehouse.dim_junk_lab_test_seq')
-, lab_test_result_code VARCHAR(20)
-, lab_test_result_description VARCHAR(100)
-, lab_test_status_code VARCHAR(20)
-, lab_test_status_description VARCHAR(100)
+, src_lab_test_result_id INTEGER
+, lab_test_result_code VARCHAR(255)
+, lab_test_result_description VARCHAR(255)
+, src_lab_test_status_id INTEGER
+, lab_test_status_code VARCHAR(255)
+, lab_test_status_description VARCHAR(255)
+, src_lab_test_type_id INTEGER
+, test_name VARCHAR(255)
 )
 ;
 alter table warehouse.dim_junk_lab_test add primary key (dw_junk_lab_test_id);
+
+
+create sequence warehouse.dim_organism_seq;
+CREATE TABLE warehouse.dim_organism
+(
+  dw_organism_id integer not null default nextval('warehouse.dim_organism_seq')
+, src_id INTEGER
+, organism_name VARCHAR(255)
+, snomed_name varchar(255)
+, snomed_code varchar(255)
+, snomed_id varchar(255)
+);
+alter table warehouse.dim_organism add primary key (dw_organism_id);
+
+create sequence warehouse.fact_lab_test_seq;
+CREATE TABLE warehouse.fact_lab_test
+(
+  dw_fact_lab_test_id integer not null default nextval('warehouse.fact_lab_test_seq')
+, dw_collection_date_id INTEGER
+, dw_junk_lab_test_id INTEGER
+, dw_lab_test_date_id INTEGER
+, dw_organism_id INTEGER
+)
+;
+alter table warehouse.fact_lab_test add primary key (dw_fact_lab_test_id);
 
