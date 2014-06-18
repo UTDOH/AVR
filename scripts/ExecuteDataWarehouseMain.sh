03cae3b765b6ed9c369c5d666fb39d3d8df8244e
Trevor MacDuff <tmacduff@gmail.com>
2014-06-18 15:57:33 -0600

    Fix timestamp

--- scripts/ExecuteDataWarehouseMain.sh
+++ scripts/ExecuteDataWarehouseMain.sh
@@ -2,4 +2,4 @@
 
 cd /var/local/pdi/
 
-./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/load_data_warehouse_main.kjb > /var/log/pdi/load_data_warehouse_main_$(date + "%Y%m%d%H%M%S").log 2>&1
+./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/load_data_warehouse_main.kjb > /var/log/pdi/load_data_warehouse_main_$(date +"%Y%m%d%H%M%S").log 2>&1
