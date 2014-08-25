a89aef87c07c22f6e05303e703caa85aa5098628
Trevor MacDuff <tmacduff@gmail.com>
2014-08-25 16:13:47 -0600

    Add sql

---
+++ pentaho/datawarehouse/etl/Fixup/add_dim_indexes_to_facts.sql
@@ -0,0 +1,61 @@
+create index idx_fe_dw_event_onset_date_id on fact_event(dw_event_onset_date_id);
+
+  create index idx_fe_dw_outbreak_id on fact_event (dw_outbreak_id);
+  
+  create index idx_fe_dw_investigation_completed_by_state_date_id on fact_event (dw_investigation_completed_by_state_date_id);
+  
+  create index idx_fe_dw_first_reported_ph_date_id on fact_event (dw_first_reported_ph_date_id);
+  
+  create index idx_fe_dw_investigation_stated_date_id on fact_event (dw_investigation_stated_date_id);
+  
+  create index idx_fe_dw_first_reported_to_clinician_date_id on fact_event (dw_first_reported_to_clinician_date_id);
+  
+  create index idx_fe_dw_lhd_case_status_id on fact_event (dw_lhd_case_status_id);
+  
+  create index idx_fe_dw_investigator_id on fact_event (dw_investigator_id);
+  
+  create index idx_fe_dw_workflow_id on fact_event (dw_workflow_id);
+  
+  create index idx_fe_dw_jurisdiction_id on fact_event (dw_jurisdiction_id);
+  
+  create index idx_fe_dw_junk_status_type_id on fact_event (dw_junk_status_type_id);
+  
+  create index idx_fe_dw_disease_id on fact_event (dw_disease_id);
+  
+  create index idx_fe_dw_location_id on fact_event (dw_location_id);
+  
+  create index idx_fe_dw_diagnosed_date_id on fact_event (dw_diagnosed_date_id);
+  
+  create index idx_fe_dw_person_id on fact_event (dw_person_id);
+
+create index idx_fe_dw_event_onset_date_id on fact_event(dw_event_onset_date_id);
+
+  create index idx_fe_dw_outbreak_id on fact_event (dw_outbreak_id);
+  
+  create index idx_fe_dw_investigation_completed_by_state_date_id on fact_event (dw_investigation_completed_by_state_date_id);
+  
+  create index idx_fe_dw_first_reported_ph_date_id on fact_event (dw_first_reported_ph_date_id);
+  
+  create index idx_fe_dw_investigation_stated_date_id on fact_event (dw_investigation_stated_date_id);
+  
+  create index idx_fe_dw_first_reported_to_clinician_date_id on fact_event (dw_first_reported_to_clinician_date_id);
+  
+  create index idx_fe_dw_lhd_case_status_id on fact_event (dw_lhd_case_status_id);
+  
+  create index idx_fe_dw_investigator_id on fact_event (dw_investigator_id);
+  
+  create index idx_fe_dw_workflow_id on fact_event (dw_workflow_id);
+  
+  create index idx_fe_dw_jurisdiction_id on fact_event (dw_jurisdiction_id);
+  
+  create index idx_fe_dw_junk_status_type_id on fact_event (dw_junk_status_type_id);
+  
+  create index idx_fe_dw_disease_id on fact_event (dw_disease_id);
+  
+  create index idx_fe_dw_location_id on fact_event (dw_location_id);
+  
+  create index idx_fe_dw_diagnosed_date_id on fact_event (dw_diagnosed_date_id);
+  
+  create index idx_fe_dw_person_id on fact_event (dw_person_id);
+ 
+ 
