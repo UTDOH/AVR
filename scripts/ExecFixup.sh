fb8e87cb2140606a96092a63dce7543e3f8adbd3
Trevor MacDuff <tmacduff@gmail.com>
2014-08-24 20:08:15 -0600

    Fix paths
    Signed-off-by: Trevor MacDuff <tmacduff@gmail.com>

--- scripts/ExecFixup.sh
+++ scripts/ExecFixup.sh
@@ -2,7 +2,7 @@
 
 cd /var/local/pdi/
 
-./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/add_person_to_fact_event_main.kjb > /var/log/pdi/add_person_to_fact_event_main_$(date +"%Y%m%d%H%M%S").log 2>&1
+./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/Fixup/add_person_to_fact_event_main.kjb > /var/log/pdi/add_person_to_fact_event_main_$(date +"%Y%m%d%H%M%S").log 2>&1
 
-./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/add_dim_indexes_to_facts.kjb > /var/log/pdi/add_dim_indexes_to_facts_$(date +"%Y%m%d%H%M%S").log 2>&1
+./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/Fixup/add_dim_indexes_to_facts.kjb > /var/log/pdi/add_dim_indexes_to_facts_$(date +"%Y%m%d%H%M%S").log 2>&1
 
