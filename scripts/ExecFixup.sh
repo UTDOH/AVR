64e7cc7854b63f2b8881a62104239a8a181f86ce
Trevor MacDuff <tmacduff@gmail.com>
2014-08-24 20:03:16 -0600

    Add fixup shell script - fix spaces

---
+++ scripts/ExecFixup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /var/local/pdi/
+
+./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/add_person_to_fact_event_main.kjb > /var/log/pdi/add_person_to_fact_event_main_$(date +"%Y%m%d%H%M%S").log 2>&1
+
+./kitchen.sh -file /var/local/project/AVR/pentaho/datawarehouse/etl/add_dim_indexes_to_facts.kjb > /var/log/pdi/add_dim_indexes_to_facts_$(date +"%Y%m%d%H%M%S").log 2>&1
